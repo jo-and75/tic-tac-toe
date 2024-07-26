fa9c6b0a437b52e4292060cf99224bdac2a892ce
jo-and75 <jaotika@gmail.com>
2024-07-26 19:45:55 +0300

    Add method to allow players to see position of symbols

--- lib/DrawBoard.rb
+++ lib/DrawBoard.rb
@@ -1,7 +1,9 @@
+
 class DrawBoard
   attr_accessor :board
+
   def initialize
-    @board = Array.new(9, ' ') 
+    @board = Array.new(9, ' ')
   end
 
   def display_board
@@ -10,5 +12,16 @@ class DrawBoard
     puts "#{@board[3]} | #{@board[4]} | #{@board[5]}"
     puts '----------'
     puts "#{@board[6]} | #{@board[7]} | #{@board[8]}"
+  end 
+
+  def set_position(index, symbol)
+    if @board[index] == " " 
+      @board[index] = symbol  
+      display_board
+      return true 
+    else  
+      puts "Position already taken. Please try again."
+      return false
+    end
   end 
 end
--- lib/main.rb
+++ lib/main.rb
@@ -1,18 +1,1 @@
-# frozen_string_literal: true
 
-class Person
-  attr_accessor :name
-
-  def initialize(name)
-    @name = name
-  end
-end
-
-class Player < Person
-  attr_accessor :sign
-
-  def initialize(name, sign)
-    super(name)
-    @sign = sign
-  end
-end
