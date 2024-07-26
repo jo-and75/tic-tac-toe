2b441a147b8b4452b716597d9841b5b2668e1d72
jo-and75 <jaotika@gmail.com>
2024-07-26 19:54:09 +0300

    Create Game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,10 +1,8 @@
-# frozen_string_literal: true
-
 require_relative 'DrawBoard'
-class Game
-  def initialize
+class Game  
+  def initialize  
     loop do
-      print 'Player 1, PLEASE CHOOSE YOUR PLAYER NAME: '
+    print 'Player 1, PLEASE CHOOSE YOUR PLAYER NAME: '
       @player_1 = gets.upcase.chomp
       print 'Player 2, PLEASE CHOOSE YOUR PLAYER NAME: '
       @player_2 = gets.upcase.chomp
@@ -12,21 +10,21 @@ class Game
     end
     @board_drawer = DrawBoard.new
     select_symbols
-  end
+  end 
 
-  def select_symbols
+  def select_symbols  
     loop do
       print "#{@player_1}, please choose your symbol (X or O): "
-      @player_1_symbol = gets.downcase.chomp
-      print "#{@player_2}, please choose your symbol (X or O): "
-      @player_2_symbol = gets.downcase.chomp
+        @player_1_symbol = gets.downcase.chomp
+        print "#{@player_2}, please choose your symbol (X or O): "
+        @player_2_symbol = gets.downcase.chomp
 
-      if @player_1_symbol != @player_2_symbol && ((@player_1_symbol == 'x' || @player_1_symbol == 'o') && (@player_2_symbol == 'x' || @player_2_symbol == 'o'))
-        break
-      end
-
-      puts "Symbols must be either 'X' or 'O' and must be different. Please reselect."
-    end
+        if @player_1_symbol != @player_2_symbol && ((@player_1_symbol == "x" || @player_1_symbol == "o") && (@player_2_symbol == "x" || @player_2_symbol == "o"))
+          break
+        else
+          puts "Symbols must be either 'X' or 'O' and must be different. Please reselect."
+        end
+    end  
     player_moves
-  end
-end
+  end 
+end 
